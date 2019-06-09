86f7abfa2295a91632877048b45f591fdbdfed86
Danko Petrovic <dankopetrovic.bgd@gmail.com>
2019-06-09 21:16:27 +0200

    remove unnecessary css class

--- client/src/components/navigation/LeftMenu.js
+++ client/src/components/navigation/LeftMenu.js
@@ -3,20 +3,14 @@ import { Link } from '@reach/router';
 import { Menu } from 'antd';
 
 function LeftMenu({ mode, activeSide, setActiveSide }) {
-  const underlineStyle = activeSide === 'right' ? { borderBottom: 'none' } : null;
   const underlineClass = activeSide === 'right' ? 'no-underline' : null;
 
   return (
     <Menu mode={mode}>
-      <Menu.Item key='home' onClick={() => setActiveSide('left')} style={underlineStyle} className={underlineClass}>
+      <Menu.Item key='home' onClick={() => setActiveSide('left')} className={underlineClass}>
         <Link to='/'>Home</Link>
       </Menu.Item>
-      <Menu.Item
-        key='search-pets'
-        onClick={() => setActiveSide('left')}
-        style={underlineStyle}
-        className={underlineClass}
-      >
+      <Menu.Item key='search-pets' onClick={() => setActiveSide('left')} className={underlineClass}>
         <Link to='/search'>Search Pets</Link>
       </Menu.Item>
     </Menu>
--- client/src/components/navigation/RightMenu.js
+++ client/src/components/navigation/RightMenu.js
@@ -3,18 +3,17 @@ import { Link } from '@reach/router';
 import { Menu, Icon } from 'antd';
 
 function RightMenu({ mode, activeSide, setActiveSide }) {
-  const underlineStyle = activeSide === 'left' ? { borderBottom: 'none' } : null;
   const underlineClass = activeSide === 'left' ? 'no-underline' : null;
 
   return (
     <Menu mode={mode}>
-      <Menu.Item key='signup' onClick={() => setActiveSide('right')} style={underlineStyle} className={underlineClass}>
+      <Menu.Item key='signup' onClick={() => setActiveSide('right')} className={underlineClass}>
         <Link to='signup'>
           <Icon type='user-add' />
           Signup
         </Link>
       </Menu.Item>
-      <Menu.Item key='login' onClick={() => setActiveSide('right')} style={underlineStyle} className={underlineClass}>
+      <Menu.Item key='login' onClick={() => setActiveSide('right')} className={underlineClass}>
         <Link to='login'>
           <Icon type='login' />
           Login
